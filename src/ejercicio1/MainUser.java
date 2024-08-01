565655f90c146b8d9f6969b47ede496285f1e8bb
Xiomara Ocampo <xocampo_165@cue.edu.co>
2024-07-31 21:16:29 -0500

    seguimiento 2 java corte 1, modificado con constructor.

--- src/ejercicio1/MainUser.java
+++ src/ejercicio1/MainUser.java
@@ -5,14 +5,12 @@ import javax.swing.*;
 
 public class MainUser {
     public static void main(String[] args) {
-        // Obtener el input del usuario a través de cuadros de diálogo
         String inputUser = JOptionPane.showInputDialog("Enter user: ");
         String inputPass = JOptionPane.showInputDialog("Enter password: ");
 
-        // Crear una instancia de User con los datos introducidos
         User user = new User(inputUser, inputPass);
 
-        // Imprimir resultados en la consola
+
         System.out.println(user.accessValidation());
         System.out.println(user.checkStatus());
     }
